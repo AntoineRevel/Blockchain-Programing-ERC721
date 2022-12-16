27da3a28da6defaf2e2764568d541b5002dcd11c
Antoine Revel <antoine.revel@gmail.com>
2022-12-16 23:20:05 +0100

    first

---
+++ src/component/pages/menu.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Menu = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <h1 className="pageTitle">Menu</h1>
+        <div className="MenuSection">
+            <button className="button" onClick={() => navigate("/erc721_ux/ChainInfo")}>Chain Info</button>
+            <button className="button" onClick={() => navigate("/erc721_ux/fakeBayc")}>fakeBayc</button>
+            <button className="button" onClick={() => navigate("/erc721_ux/fakeNefturians")}>fakeNefturians</button>
+            <button className="button" onClick={() => navigate("/erc721_ux/fakeMeebits")}>fakeMeebits</button>
+        </div>
+        </>
+        
+    )
+};
+
+export default Menu;
